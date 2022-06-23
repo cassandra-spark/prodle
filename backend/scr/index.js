c24a4060fc098110caaf43d7324ee5d8417a2b5f
navesaurus <anyatmnt@gmail.com>
2022-06-23 15:59:34 +0200

    Added login and register (not working yet)

--- backend/scr/index.js
+++ backend/scr/index.js
@@ -5,13 +5,18 @@ const MongoClient = require('mongodb').MongoClient
 const ObjectId = require('mongodb').ObjectId
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 
 // Create Express app
 const app = express()
+const saltRounds = 10;
 
 // Add CORS to all routes and methods
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Enable parsing of JSON bodies
 app.use(express.json())
@@ -31,25 +36,48 @@ let db
 
 //TO-DO:Login also not idea what to do
 app.route('/login').post(async(req, res) =>{
-
-
-
-  
- 
-
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    console.log(user);
+    if (user) {
+      const cmp = await bcrypt.compare(req.body.password, user.password);
+      if (cmp) {
+        //   ..... further code to maintain authentication like jwt or sessions
+        res.send("Auth Successful");
+      } else {
+        res.send("Wrong username or password.");
+      }
+    } else {
+      res.send("Wrong username or password.");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server error Occured");
+  }
 })
 
 
 //TO_DO: Sign up lol idk whats up
 app.route('/user').post(async (req, res) => {
   try{
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    console.log(salt)
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds)
     console.log(hashedPassword)
-    const user = {...req.body, password: hashedPassword}
-    const result = await db.collection(userCollection).insertOne(user)
-    res.status(200).send()
-  }catch{
+    const insertResult = await user.create({
+      username: req.body.username,
+      password: hashedPassword,
+      fullname: req.body.fullname,
+      type: req.body.type,
+      birthdate: req.body.birthdate,
+      degree: req.body.degree,
+      major: req.body.major,
+      description: req.body.description,
+      email_address: req.body.email_address,
+      saved: "[]" 
+    })
+    //const user = {...req.body, password: hashedPassword}
+    //const result = await db.collection(userCollection).insertOne(user)
+    res.status(200).send(insertResult)
+  }catch (error) {
     res.status(500)
   }
   })
