f38429bc814d51b6fcc7eda970d0d07b82731b54
Jean Qussa <jean.qussa@gmail.com>
2022-07-10 20:55:46 +0200

    Add link to action button on home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -217,12 +217,13 @@ export default function Home() {
                 </p>
                 <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                   <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:gap-5">
-                    <a
-                      href="#"
-                      className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-orange-700 bg-white hover:bg-orange-50 sm:px-8"
-                    >
-                      Explore Projects
-                    </a>
+                    <Link href="/projects">
+                      <a
+                        className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-orange-700 bg-white hover:bg-orange-50 sm:px-8"
+                      >
+                        Explore Projects
+                      </a>
+                    </Link>
                   </div>
                 </div>
               </div>
